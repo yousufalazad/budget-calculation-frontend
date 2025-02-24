ed64a69b42f8f053e0698ea4ff0a1f8e7e553b30
ourazad <ourazad@gmail.com>
2025-02-24 15:40:16 +0000

    Register update

--- src/views/Auth/Register.vue
+++ src/views/Auth/Register.vue
@@ -60,7 +60,7 @@
           <button
             type="submit"
             class="mt-6 w-full bg-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-700 transition-all"
-          >
+            >
             Register
           </button>
         </form>
